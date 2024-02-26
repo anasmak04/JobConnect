629551c45856b3aa9a0221cabde07237b46b780d
codeOplomo <anasalk@gmail.com>
2024-02-26 22:28:09 +0100

    web conflit

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,14 @@ Route::get('/candidat/fill-representer-info', [CandidatController::class, 'showR
 Route::post('/candidat/save-representer-info', [CandidatController::class, 'saveRepresenterInfo'])->name('candidat.save.representer.info');
 Route::get('/candidat/candidat_profile', [CandidatController::class, 'index'])->name('candidat.profile');
 Route::get('/job-offers', [JobOfferController::class, 'index'])->name('job_offers.index');
+Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
+Route::put('/user/{user}/update-company', [RepresenterController::class, 'updateRepresenterCompany'])->name('user.update.company');
+Route::get('/job-offers', [JobOfferController::class, 'index'])->name('job_offers.index');
 Route::get('/job-offers/{job_offer}', [JobOfferController::class, 'show'])->name('job_offers.show');
 Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
 Route::get('/companies/{company}/job-offers', [CompanyController::class, 'showJobOffers'])->name('companies.job_offers');
 
-
 // Route::resource("user", UserController::class);
 // Route::resource("skill", SkillController::class);
 // Route::resource("city", CityController::class);
-// Route::put('/user/{user}/update-company', [RepresenterController::class, 'updateRepresenterCompany'])->name('user.update.company');
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
