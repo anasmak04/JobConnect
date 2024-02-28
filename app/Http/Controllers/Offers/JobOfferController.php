54e4fffbc980dedb21943e0ea65f71230b4b3d0a
anasmak04 <anasdev8@gmail.com>
2024-02-28 09:06:17 +0100

    update jobOffercontroller

--- app/Http/Controllers/Offers/JobOfferController.php
+++ app/Http/Controllers/Offers/JobOfferController.php
@@ -6,7 +6,6 @@ namespace App\Http\Controllers\Offers;
 use App\Http\Controllers\Controller;
 use App\Models\JobOffer;
 use App\Models\City;
-use App\Models\Secteur;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -17,34 +16,18 @@ class JobOfferController extends Controller
     public function index(Request $request)
     {
         $searchTerm = $request->input('searchTerm');
-        $secteur_id = $request->input('secteur_id');
-        $secteurs = Secteur::all(); // Consider if this is necessary based on your UI needs.
-        $user = User::find(auth()->id()); // Moved outside to ensure it's defined for the view.
-
-        // Start with a basic query that can be modified based on conditions.
-        $jobOffersQuery = JobOffer::query();
-
-        if ($searchTerm) {
-            $jobOffersQuery->where('title', 'LIKE', "%{$searchTerm}%")
-                ->orWhere('description', 'LIKE', "%{$searchTerm}%");
+        if($searchTerm){
+            $jobOffers = JobOffer::where('title', 'LIKE', "%{$searchTerm}%")
+                ->OrWhere('description', 'LIKE', "%{$searchTerm}%")->get();
         }
-
-        if ($secteur_id) {
-            $jobOffersQuery->where("secteur_id", $secteur_id);
+        else {
+            $user = User::find(auth()->id());
+            $userJobOffers = $user->jobOffers()->withPivot('offer_status')->get()->keyBy('id');
+            $jobOffers = JobOffer::with(['company', 'secteur'])->get();
         }
+        return view('jobOffers.index', compact('jobOffers', "searchTerm", "user", "userJobOffers"));
 
-        // If no specific filters are applied, eager load relationships (optional based on your needs).
-        if (!$searchTerm && !$secteur_id) {
-            $jobOffersQuery->with(['company', 'secteur']);
-        }
-
-        $jobOffers = $jobOffersQuery->get();
-        $userJobOffers = $user->jobOffers()->withPivot('offer_status')->get()->keyBy('id'); // Consider fetching this conditionally if it's heavy.
-
-        return view('jobOffers.index', compact('jobOffers', 'searchTerm', 'user', 'userJobOffers', 'secteurs'));
     }
-
-
 
     public function show(JobOffer $job_offer)
     {
